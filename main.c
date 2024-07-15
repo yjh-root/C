25f65a03301edc56ba6ff24e64aa3196e822f2ce
admin <yin_jia_hao@outlook.com>
2024-07-15 17:21:45 +0800

    C基础-2.5OJ网站使用-1% （大模块-视频名称-视频进度）

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+#define PIE 3+2
 int main() {
-    printf("你好，世界\n");
+    int i;
+    int ret;
+    char c;
+    float f;
+//    scanf("%d",&i);
+//    printf("i=%d\n",i);
+//    fflush(stdin);
+    ret=scanf("%d %c %f",&i,&c,&f);
+    printf("c=%c\n",c);
+//    scanf("%f",&f);
+//    printf("f=%f\n",f);
     return 0;
 }
